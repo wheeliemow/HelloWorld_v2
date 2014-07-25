e19efe0b270a59d707d6b4080e4ff9ea7d81a388
Andy Jandt <ajandt@smp.org>
2014-07-25 13:02:03 -0500

    Both iOS and Android versions
    This is a version that contains both iOS and Android versions of the
    app in one solution.

---
+++ HelloWorld.iOS/UI/HelloView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace HelloWorld.iOS
+{
+	public class HelloView :UIView
+	{
+		public UITextView txtHello;
+
+
+		public HelloView ()
+		{
+			txtHello = new UITextView (new RectangleF (60, 20, 100, 500));
+
+			txtHello.TextColor = UIColor.Blue;
+			txtHello.Text = "Hello World";
+			this.AddSubview (txtHello);
+
+
+		}
+	}
+}
+
---
+++ HelloWorld.iOS/UI/HomeViewController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace HelloWorld.iOS
+{
+	public class HomeViewController :UIViewController
+	{
+		public HomeViewController ()
+		{
+			HelloView view = new HelloView ();
+			view.BackgroundColor = UIColor.White;
+
+
+			this.View = view;
+
+
+		}
+	}
+}
+
